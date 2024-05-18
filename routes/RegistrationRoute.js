feb48e3c651ad94f950ccf1724eb2a3e9521c568
Ivan Negohe <inegohe@student.refactory.academy>
2024-05-18 18:50:19 +0300

    remove redudant admin registration routes

--- routes/RegistrationRoute.js
+++ routes/RegistrationRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-console.log(__dirname); // This will help in debugging the current directory path
+// console.log(__dirname); // This will help in debugging the current directory path
 
 // Adjust the model imports as per your project structure
 // const adminModelPath = path.join(__dirname, "../models/adminmodel");
@@ -14,22 +14,22 @@ console.log(`Loading Registration model from: ${registrationModelPath}`);
 const registrationmodel = require(registrationModelPath);
 
 // GET request for admin registration
-router.get("/adminregistration", (req, res) => {
-  res.render(path.join(__dirname, "../views/adminregistration"));
-});
+// router.get("/adminregistration", (req, res) => {
+//   res.render(path.join(__dirname, "../views/adminregistration"));
+// });
 
-// POST request for admin registration
-router.post("/adminregistration", async (req, res, next) => {
-  try {
-    const admin = new Admin(req.body);
-    console.log(admin);
-    await Admin.register(admin, req.body.password);
-    res.redirect("/index");
-  } catch (error) {
-    res.status(400).send("Couldn't register admin");
-    console.log(error);
-  }
-});
+// // POST request for admin registration
+// router.post("/adminregistration", async (req, res, next) => {
+//   try {
+//     const admin = new Admin(req.body);
+//     console.log(admin);
+//     await Admin.register(admin, req.body.password);
+//     res.redirect("/index");
+//   } catch (error) {
+//     res.status(400).send("Couldn't register admin");
+//     console.log(error);
+//   }
+// });
 
 // GET request for user registration
 router.get("/adminregistration", (req, res) => {
