dc1c1afc30dcf6e7fdd33fd08eedb801ca258f78
Ivan Negohe <inegohe@student.refactory.academy>
2024-05-18 22:44:39 +0300

    made corrections in the sitter model

--- models/sitterreg.js
+++ models/sitterreg.js
@@ -1,64 +1,58 @@
 const mongoose = require("mongoose");
 
-const sitterregSchema =new mongoose.Schema({
-name:{
-     type:String,
-    trim:true
+const sitterregSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+  },
+  dob: {
+    type: Date,
+    trim: true,
+  },
+  nin: {
+    type: String,
+    trim: true,
+  },
 
-},
-location:{
-    type:String,
-    trim:true
-
-},
-dob:{
-
-type:Date,
-    trim:true
-},
-
-gender:{
-type:String,
- trim:true
-},
-nextofkin:{
-type:String,
- trim:true
- },
- recommendername:{
-
- type:String,
- trim:true
- },
- religion:{
-
- type:String,
-    trim:true
- },
-    educationlevel:{
-
-    type:String,
-     trim:true
-                    },
-    sitternumber:{
-
-    type:String,
-    trim:true
-},
-departuretime:{
-
-    type:String,
-    trim:true
-},
-personTaking:{
-
-    type:String,
-    trim:true
-},
-                                                   contacts:{
-                            type:Number,
-                            trim:true
-
-                        }
-})
-module.exports=mongoose.model("sitterreg", sitterregSchema)
+  gender: {
+    type: String,
+    trim: true,
+  },
+  nextofkin: {
+    type: String,
+    trim: true,
+  },
+  recommendername: {
+    type: String,
+    trim: true,
+  },
+  religion: {
+    type: String,
+    trim: true,
+  },
+  educationlevel: {
+    type: String,
+    trim: true,
+  },
+  sitternumber: {
+    type: String,
+    trim: true,
+  },
+  departuretime: {
+    type: String,
+    trim: true,
+  },
+  personTaking: {
+    type: String,
+    trim: true,
+  },
+  contacts: {
+    type: Number,
+    trim: true,
+  },
+});
+module.exports = mongoose.model("sitterreg", sitterregSchema);
